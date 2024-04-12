9f19e619b327857d2ee47da1d13691bf16cc16a6
Gustavo Dias <90855105+guusdias@users.noreply.github.com>
2024-04-11 21:54:29 -0300

    Add files via upload

---
+++ forms.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="./index.php" method="POST">
+    <input name="name" type="text">
+    <button type="submit">Click me!</button>
+  </form>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+
+<head>
+  <title>Example</title>
+</head>
+
+<body><H1>caiu no bait,</H1>
+  <?=$_POST['name']?>
+</body>
+
+</html>
